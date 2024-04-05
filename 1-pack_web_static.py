0c3bba80b83e664423b27ed1302d0d4b0892c4e1
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-04-05 20:38:45 +0100

    new

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,21 +1,24 @@
-#!/usr/bin/env bash
-# Write a Fabric script that generates a .tgz archive from the contents
-# of the web_static folder of your AirBnB Clone repo, using the
-# function do_pack.
-
+#!/usr/bin/python3
+"""
+Write a Fabric script that generates a .tgz archive from the contents
+of the web_static folder of your AirBnB Clone repo, using the
+function do_pack.
+"""
 from fabric.api import local
 from datetime import datetime
 
 
+"""Define do_pack module"""
 def do_pack():
     try:
         local("mkdir -p versions")
 
+        """Craete a timestamp for file"""
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
         archive_name = "web_static_{}.tgz".format(timestamp)
 
-        # Create .tgz archive from web_static folder
+        """Create .tgz archive from web_static folder"""
         local("tar -cvzf versions/{} web_static".format(archive_name))
 
         return "versions/{}".format(archive_name)
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,16 @@
-#!/usr/bin/env bash
-# Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy
+#!/usr/bin/python3
+"""
+Write a Fabric script (based on the file 1-pack_web_static.py
+that distributes an archive to your web servers, using the
+function do_deploy
+"""
+from fabric.api import *
+import os
+
+
+env.user = 'ubuntu'
+env.hosts = ['']
 def do_deploy(archive_path):
     try:
-        dist = local('python3 ')
+        dist = local('python3 1-pack_web_static.py --fullname', capture=True).strip()
+        filename = 
