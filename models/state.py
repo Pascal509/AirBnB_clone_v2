66b836bd7ce71a03165158c12c17cd3f88736431
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-04-22 06:15:08 +0100

    changes

--- models/state.py
+++ models/state.py
@@ -3,6 +3,18 @@
 from models.base_model import BaseModel
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+# class State(BaseModel):
+   #  """ State class """
+    # name = ""
+from models import storage, city
+
+class State(BaseModel, Base):
+    if storage_t != 'db':
+
+
+        @property
+        def cities(self):
+            """Return the list of City instances with state_id equals to the current State.id"""
+            all_cities = storage.all(City)
+            return [city for city in all_cities.values() if city.state_id == self.id]
+
